8e2c1a4ccdb2e35cf7312aef8a9bdeff92d8aa09
saddab <saddabakhtar17@gmail.com>
2026-04-15 23:52:40 +0530

    feat(patient-service): add Patient entity and database config

---
+++ patient-service/src/main/java/com/sadcodes/patientservice/mode/Patient.java
@@ -0,0 +1,42 @@
+package com.sadcodes.patientservice.mode;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "patients")
+public class Patient {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotNull
+    private String address;
+
+    @NotNull
+    private LocalDateTime dataOfBirth;
+
+    @NotNull
+    private LocalDateTime registerDate;
+}
